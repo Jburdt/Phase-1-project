4b4df9adff3a275f490d0a0206f5e2cb37d2ded9
Josh Burd <Jburdt@gmail.com>
2022-07-27 00:56:51 -0400

    mostly done

--- Javascripts/index.js
+++ Javascripts/index.js
@@ -1,11 +1,31 @@
-/* overall idea: when button is clicked it posts a new joke
 
-when: on click (click) event
+//NODE GETTERS
+let insideDive = document.getElementById('inside')
+let jokeButton = document.getElementById('jokeButton')
+let pTag = document.getElementById('pTag')
 
-cause: click
+//PAGE LOAD
+document.addEventListener('DOMContentLoaded', getRandomJokes)
+
+//EVENT LISTNERS
+jokeButton.addEventListener('click', getRandomJokes)
+
+//FETCH API JOKES 
+function getRandomJokes() {
+    const apiData = fetch('https://icanhazdadjoke.com/', {
+       headers: {
+           'Accept': 'application/json'
+        }
+    })
+    
+    .then(res => res.json())
+    //gets the res and pass it to another function to .json and get json format and
+    //get an object
+    .then(apiData => pTag.innerText = apiData.joke)
+    .catch(error => console.log(error, 'not working'))
+  
+}
+ 
 
 
-effect: render joke to page
 
-
-*/
