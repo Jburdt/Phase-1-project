ecb3b4b7c1a99655a8546fe60fc3d40daffc3288
Josh Burd <Jburdt@gmail.com>
2022-07-28 21:59:24 -0400

    Added heart

--- Javascripts/index.js
+++ Javascripts/index.js
@@ -4,7 +4,7 @@ let insideDive = document.getElementById('inside')
 let jokeButton = document.getElementById('jokeButton')
 let pTag = document.getElementById('pTag')
 let likeButton = document.getElementById('like')
-
+const button = document.querySelector(".heart-like-button");
 
 //PAGE LOAD
 document.addEventListener('DOMContentLoaded', getRandomJokes)
@@ -12,6 +12,14 @@ document.addEventListener('DOMContentLoaded', getRandomJokes)
 //EVENT LISTNERS
 jokeButton.addEventListener('click', getRandomJokes)
 
+//SHOWS LIKED HEART
+button.addEventListener("click", () => {
+  if (button.classList.contains("liked")) {
+    button.classList.remove("liked");
+  } else {
+    button.classList.add("liked");
+  }
+});
 
 //FETCH API JOKES 
 function getRandomJokes() {
@@ -20,18 +28,15 @@ function getRandomJokes() {
            'Accept': 'application/json'
         }
     })
-    
     .then(res => res.json())
     //gets the res and pass it to another function to .json and get json format and
     //get an object
     .then(apiData => pTag.innerText = apiData.joke)
     .catch(error => console.log(error, 'not working'))
-  
-}
- 
-
-
-}
+//RESETS HEART BUTTON
+     if (button.classList.contains("liked")) {
+        button.classList.remove("liked")}
+};
 
 
 
